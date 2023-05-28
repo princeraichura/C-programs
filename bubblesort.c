bdc0530755654fbcfd011493e61f0b9e01ef286f
princeraichura <raichura72@gmail.com>
2023-05-28 21:22:27 +0530

    selection sort in c

---
+++ bubblesort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// Function to select the lowest number and bring it to the beginning
+void selection(int arr[], int n)
+{
+    int min;
+    for (int i = 0; i < n - 1; i++)
+    {
+        min = i; // Assuming first element as the minimum
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[min] > arr[j])
+                min = j;
+            swap(&arr[min], &arr[i]); // Bringing the minimum element to the beginning of the array
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    selection(arr, n); // Calling the selection function
+
+    printf("sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
